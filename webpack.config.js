55d75bd22fd8ebb649819e130c1e0e38944d4d3d
tahirbhalli <tahirbhalli16@gmail.com>
2020-07-10 22:28:18 +0500

    config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,7 @@
 const Htmlwebpackplugin = require('html-webpack-plugin');
 const Minicss = require('mini-css-extract-plugin');
-const { resolve } = require('path');
 
 module.exports = {
-  configBaseDir: resolve(__dirname, 'node_modules'),
-  plugins: [
-    'stylelint-group-selectors',
-    'stylelint-csstree-validator',
-    'stylelint-no-unsupported-browser-features',
-    'stylelint-selector-no-empty',
-    'stylelint-no-unused-selectors',
-  ],
-  extends: [
-    'stylelint-config-recommended-scss',
-    'stylelint-config-recess-order',
-    'stylelint-prettier/recommended',
-    'stylelint-scss',
-    'stylelint-a11y/recommended',
-  ],
   module: {
     rules: [
       {
