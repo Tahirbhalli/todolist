930fce708f2383d41be2b8271d0cbae08080d8c0
tahirbhalli <tahirbhalli16@gmail.com>
2020-07-13 20:17:18 +0500

    delete items

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,53 @@
-// eslint-disable import/no-unresolved
-import Htmlwebpackplugin from 'html-webpack-plugin';
-import Minicss from 'mini-css-extract-plugin';
+const Htmlwebpackplugin = require('html-webpack-plugin');
+const Minicss = require('mini-css-extract-plugin');
 
-export const module = {
-  rules: [
-    {
-      test: /\.(scss|sass|css)$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader',
-      ],
-    },
-    {
-      test: /\.(png|svg|jpg|jpeg|gif)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          esModule: false,
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(scss|sass|css)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            esModule: false,
+          },
         },
       },
-    },
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-      },
-    },
-    {
-      test: /\.html$/,
-      use: [
-        {
-          loader: 'html-loader',
-          options: { minimize: true },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         },
-      ],
-    },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new Htmlwebpackplugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new Minicss({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
   ],
 };
-
-export const plugins = [
-  new Htmlwebpackplugin({
-    template: './src/index.html',
-    filename: './index.html',
-  }),
-  new Minicss({
-    filename: '[name].css',
-    chunkFilename: '[id].css',
-  }),
-];
