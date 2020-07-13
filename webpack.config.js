a5541cff61c1e5ac969cf991021739b7f53b3787
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-07-13 18:19:40 +0530

    Modify webpack config syntax to ES6

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,51 @@
-const Htmlwebpackplugin = require('html-webpack-plugin');
-const Minicss = require('mini-css-extract-plugin');
+import Htmlwebpackplugin from 'html-webpack-plugin';
+import Minicss from 'mini-css-extract-plugin';
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(scss|sass|css)$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            esModule: false,
-          },
+export const module = {
+  rules: [
+    {
+      test: /\.(scss|sass|css)$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader',
+      ],
+    },
+    {
+      test: /\.(png|svg|jpg|jpeg|gif)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          esModule: false,
         },
       },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+    },
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+      },
+    },
+    {
+      test: /\.html$/,
+      use: [
+        {
+          loader: 'html-loader',
+          options: { minimize: true },
         },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: { minimize: true },
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new Htmlwebpackplugin({
-      template: './src/index.html',
-      filename: './index.html',
-    }),
-    new Minicss({
-      filename: '[name].css',
-      chunkFilename: '[id].css',
-    }),
+      ],
+    },
   ],
 };
+
+export const plugins = [
+  new Htmlwebpackplugin({
+    template: './src/index.html',
+    filename: './index.html',
+  }),
+  new Minicss({
+    filename: '[name].css',
+    chunkFilename: '[id].css',
+  }),
+];
